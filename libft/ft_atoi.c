06872c8404bbda05e6709cde90b6d8855d234df0
dcheng093 <chengpeter37093@gmail.com>
2025-09-19 09:37:55 +0800

    stuff

---
+++ libft/ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcheng <dcheng@student.42kl.edu.my>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/19 08:11:31 by dcheng            #+#    #+#             */
+/*   Updated: 2025/09/19 08:11:31 by dcheng           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_atoi(const char *nptr)
+{
+	int				i;
+	int				sign;
+	unsigned long	result;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	while (nptr[i] == 32 || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-')
+	{
+		sign *= -1;
+		i++;
+	}
+	else if (nptr[i] == '+')
+		i++;
+	while (nptr[i] && (nptr[i] >= '0' && nptr[i] <= '9'))
+	{
+		result = (result * 10) + nptr[i] - 48;
+		i++;
+	}
+	return ((int)result * sign);
+}
